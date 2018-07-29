93caeb9a53d728ab7170510dbfccf96ba6b90bd0
notacoderr <38270548+notacoderr@users.noreply.github.com>
2018-07-29 16:24:43 +0800

    Update test.php

--- test.php
+++ test.php
@@ -19,19 +19,28 @@ class capesUI
         $form = $this->main->formapi->createSimpleForm(function (Player $player, array $data) {
 
             if (isset($data[0])){
+		    
                 $button = $data[0];
-		$this->applyCape($button, $player);
 		$cape = $this->getCapeCustomName()[$button];
                 $player->addTitle("§l§7[ ". $cape ." §7]", "§o§aYour cape has been updated");
+		    
+		if(strtolower($player->getSkinId()) === "standard_custom")
+		{
+			$this->applyMaleCape($button, $player);
+		} else {
+			$this->applyFemaleCape($button, $player);
+		}
+		    
                 return true;
             }
 			
         });
 		
         $form->setTitle('§l§fCosmetics');
+	$form->addButton("§l§fRemove Cape");
 	foreach($this->getCapeCustomName() as $cape)
 	{
-		$form->addButton($cape);
+		$form->addButton("§l§f" . $cape);
 	}
 		
         $form->sendToPlayer($player);
@@ -49,24 +58,50 @@ class capesUI
 		return $capes;
 	}
 
-	private applyCape(int $cape, Player $player) : void {
+	private applyMaleCape(int $cape, Player $player) : void {
 		switch($cape){
-			case 0:
+			case 1:
 			  $player->setSkin($player->getSkinData(), "Minecon_MineconSteveCape2016");
 			break;
-			case 1:
+			case 2:
 			  $player->setSkin($player->getSkinData(), "Minecon_MineconSteveCape2015");
 			break;
-			case 2:
+			case 3:
 			  $player->setSkin($player->getSkinData(), "Minecon_MineconSteveCape2013");
 			break;
-			case 3:
+			case 4:
 			  $player->setSkin($player->getSkinData(), "Minecon_MineconSteveCape2012");
 			break;
-			case 4:
+			case 5:
 			  $player->setSkin($player->getSkinData(), "Minecon_MineconSteveCape2011");
 			break;
+			default:
+				$player->setSkin($player->getSkinData(), "Standard_CustomSlim");
 		}
+		$player->sendSkin($this->main->getServer()->getOnlinePlayers());
+	}
+	
+	private applyFemaleCape(int $cape, Player $player) : void {
+		switch($cape){
+			case 1:
+			  $player->setSkin($player->getSkinData(), "Minecon_MineconAlexCape2016");
+			break;
+			case 2:
+			  $player->setSkin($player->getSkinData(), "Minecon_MineconAlexCape2015");
+			break;
+			case 3:
+			  $player->setSkin($player->getSkinData(), "Minecon_MineconAlexCape2013");
+			break;
+			case 4:
+			  $player->setSkin($player->getSkinData(), "Minecon_MineconAlexCape2012");
+			break;
+			case 5:
+			  $player->setSkin($player->getSkinData(), "Minecon_MineconAlexCape2011");
+			break;
+			default:
+				$player->setSkin($player->getSkinData(), "Standard_CustomSlim");
+		}
+		$player->sendSkin($this->main->getServer()->getOnlinePlayers());
 	}
 	
 }

890c4bdb73c7c584b5a5ae53da81b152fe022aa0
notacoderr <38270548+notacoderr@users.noreply.github.com>
2018-07-29 15:59:27 +0800

    Update test.php

--- test.php
+++ test.php
@@ -12,20 +12,16 @@ class capesUI
     public $main;
 					
 	public function __construct(CI $pg) {
-        $this->main = $pg;
-    }
+        	$this->main = $pg;
+    	}
 	
-	public function mainForm(Player $player)
-    {
-        $form = $this->main->formapi->createCustomForm(function (Player $player, array $data) {
+	public function mainForm(Player $player) {
+        $form = $this->main->formapi->createSimpleForm(function (Player $player, array $data) {
 
             if (isset($data[0])){
                 $button = $data[0];
-				
-				$cape = $this->getCapeActual[ $button ]; //selecting the capes
-				
-				$player->setSkin($player->getSkinData(), $cape); //setting the skin + cape
-				
+		$this->applyCape($button, $player);
+		$cape = $this->getCapeCustomName()[$button];
                 $player->addTitle("§l§7[ ". $cape ." §7]", "§o§aYour cape has been updated");
                 return true;
             }
@@ -33,30 +29,44 @@ class capesUI
         });
 		
         $form->setTitle('§l§fCosmetics');
-		$form->addDropdown("Please select a Cape:", $this->getCapeCustomName, 0); //data 0
-        $form->sendToPlayer($player);
-    }
-	
-	private getCapeCustomName() : array {
-		$tempCapes = array(
-                    "PickAxe Cape", //SteveCape2016
-                    "ProbsEnderman Cape", //"Minecon_MineconSteveCape2015", HELP MEEEH IDK TF ARE THESE
-                    "Mojang?", //"Minecon_MineconSteveCape2013",
-                    "Minecon_MineconSteveCape2012", //NAMESC 
-                    "Minecon_MineconSteveCape2011"
-					);
-					
+	foreach($this->getCapeCustomName() as $cape)
+	{
+		$form->addButton($cape);
 	}
+		
+        $form->sendToPlayer($player);
+    	}
 	
-	private getCapeActual(int $whatever) : string {
-		$trueCapes = array(
-                    "Minecon_MineconSteveCape2016",
-                    "Minecon_MineconSteveCape2015",
-                    "Minecon_MineconSteveCape2013",
-                    "Minecon_MineconSteveCape2011"
-					);
-					
-		return $trueCapes [ $whatever ];
-	}	
+	private getCapeCustomName() : array
+	{
+		$capes = array(
+                    "Enderman", //"Minecon_MineconSteveCape2016"
+                    "Iron Golem", //"Minecon_MineconSteveCape2015"
+                    "Piston", //"Minecon_MineconSteveCape2013"
+                    "Pick Axe", //"Minecon_MineconSteveCape2012"
+                    "Red Creeper" //"Minecon_MineconSteveCape2011"
+		);
+		return $capes;
+	}
+
+	private applyCape(int $cape, Player $player) : void {
+		switch($cape){
+			case 0:
+			  $player->setSkin($player->getSkinData(), "Minecon_MineconSteveCape2016");
+			break;
+			case 1:
+			  $player->setSkin($player->getSkinData(), "Minecon_MineconSteveCape2015");
+			break;
+			case 2:
+			  $player->setSkin($player->getSkinData(), "Minecon_MineconSteveCape2013");
+			break;
+			case 3:
+			  $player->setSkin($player->getSkinData(), "Minecon_MineconSteveCape2012");
+			break;
+			case 4:
+			  $player->setSkin($player->getSkinData(), "Minecon_MineconSteveCape2011");
+			break;
+		}
+	}
 	
 }
